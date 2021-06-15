445baad626e5ab774d1c9bdde4edbb486135da3d
saif ahmed <asshaik@corp.untd.com>
2021-06-15 23:56:18 +0530

    Create New files for Albums List Table View Cell
    Also add extension to UIView to add a view loaded
    from XIB as a subview.

---
+++ PhotosViewer/PhotosViewer/AlbumsList/AlbumsListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AlbumsListTableViewCell.swift
+//  PhotosViewer
+//
+//  Created by saif ahmed on 15/06/21.
+//  Copyright © 2021 Saif. All rights reserved.
+//
+
+import UIKit
+
+class AlbumsListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ PhotosViewer/PhotosViewer/Extension + UIView.swift
@@ -0,0 +1,9 @@
+//
+//  Extension + UIView.swift
+//  PhotosViewer
+//
+//  Created by saif ahmed on 15/06/21.
+//  Copyright © 2021 Saif. All rights reserved.
+//
+
+import Foundation
