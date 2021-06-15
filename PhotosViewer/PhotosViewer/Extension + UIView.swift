0f0392f5003adbbb2e9d57f63a02abd8b081f7d8
saif ahmed <asshaik@corp.untd.com>
2021-06-15 23:57:03 +0530

    Implement method to add a view loaded from XIB as a subview.

--- PhotosViewer/PhotosViewer/Extension + UIView.swift
+++ PhotosViewer/PhotosViewer/Extension + UIView.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+
+extension UIView {
+    func loadView(for name: String) -> UIView? {
+        let bundle = Bundle(for: Self.self)
+        let nib = UINib(nibName: name, bundle: bundle)
+        return nib.instantiate(withOwner: self, options: nil).first as? UIView
+    }
+    func addXib(withName name: String? = nil) {
+        let selfName = name ?? String(describing: type(of: self))
+        guard let view = loadView(for: selfName) else { return }
+        
+        addSubview(view)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        let constraints = [view.topAnchor.constraint(equalTo: topAnchor),
+                           view.bottomAnchor.constraint(equalTo: bottomAnchor),
+                           view.leadingAnchor.constraint(equalTo: leadingAnchor),
+                           view.trailingAnchor.constraint(equalTo: trailingAnchor)]
+        
+        NSLayoutConstraint.activate(constraints)
+    }
+}
+
