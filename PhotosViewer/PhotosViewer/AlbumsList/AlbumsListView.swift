d4a25c36715427dedc222c539f3ce1c391411417
saif ahmed <asshaik@corp.untd.com>
2021-06-15 21:25:55 +0530

    Add new files for AlbumsList View.

---
+++ PhotosViewer/PhotosViewer/AlbumsList/AlbumsListView.swift
@@ -0,0 +1,21 @@
+//
+//  AlbumsListView.swift
+//  PhotosViewer
+//
+//  Created by saif ahmed on 15/06/21.
+//  Copyright © 2021 Saif. All rights reserved.
+//
+
+import UIKit
+
+class AlbumsListView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
