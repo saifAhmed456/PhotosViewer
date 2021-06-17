f7328ffbfeefea8e74b938c7d212a4b91bbf073c
saif ahmed <asshaik@corp.untd.com>
2021-06-18 02:59:25 +0530

    Download the image once photoURL subject is set
    it binds the downloaded image to imageRelay
    which in turn is bound to photo detail view's image view's
    image.

--- PhotosViewer/PhotosViewer/PhotoDetail/PhotoDetailViewModel.swift
+++ PhotosViewer/PhotosViewer/PhotoDetail/PhotoDetailViewModel.swift
@@ -7,4 +7,54 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+import RxSwiftExt
+
+protocol PhotoDetailFlowCoordinating {
+    
+}
+class PhotoDetailViewModel {
+    let flowCoordinator : PhotoDetailFlowCoordinating
+    let photoURLSubject = PublishSubject<URL>()
     let networking = Networking.shared
+    let disposeBag = DisposeBag()
+    var photoTitle : String?
+    let imageRelay = BehaviorRelay<UIImage?>(value : nil)
+    let animateSpinner = BehaviorRelay<Bool>(value: false)
+    init(flowCoordinator : PhotoDetailFlowCoordinating) {
+        self.flowCoordinator = flowCoordinator
+        setupBindings()
+    }
+    func setupBindings(with view : PhotoDetailViewControllerProtocol) {
+        imageRelay
+            .bind(to: view.imageBinder)
+            .disposed(by: disposeBag)
+        
+        Observable.of(photoTitle)
+            .bind(to: view.navItemTitle)
+            .disposed(by: disposeBag)
+        
+        animateSpinner
+            .bind(to: view.animateSpinner)
+            .disposed(by : disposeBag)
+    }
+    func setupBindings() {
+        photoURLSubject
+            .flatMap {[weak self] (url) -> Observable<UIImage?> in
+                guard let `self` = self else { return .empty()}
+                self.animateSpinner.accept(true)
+                let endpoint = PhotoViewerEndpoint.photoData(url: url)
+                return self.networking.request(endpoint: endpoint)
+                    .asObservable()
+                    .do(onDispose : {[weak self] () in
+                        self?.animateSpinner.accept(false)
+                        
+                    })
+                    .map{UIImage(data :$0.data)}
+                
+            }
+            .bind(to: imageRelay)
+            .disposed(by: disposeBag)
+    }
+}
