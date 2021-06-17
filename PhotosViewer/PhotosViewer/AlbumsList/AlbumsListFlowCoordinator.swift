50076bafc97442fb23bd92eb9d7760c904da7fba
saif ahmed <asshaik@corp.untd.com>
2021-06-17 10:55:08 +0530

    Create a FlowCoordinator for AlbumsList
    This flowcoordinator handles the navigation
    between the view controllers and creating
    AlbumsListViewController

--- PhotosViewer/PhotosViewer/AlbumsList/AlbumsListFlowCoordinator.swift
+++ PhotosViewer/PhotosViewer/AlbumsList/AlbumsListFlowCoordinator.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+import UIKit
+class AlbumsListFlowCoordinator : AlbumsListFlowCoordinating {
+    var presenter : UIViewController?
+    var navigator : UINavigationController?
+    
+   static  func prepareAlbumsViewController(with navigator : UINavigationController?) -> UIViewController {
+        let storyBoard = UIStoryboard(name: "AlbumsListStoryboard", bundle: nil)
+    guard   let albumsViewController = storyBoard.instantiateInitialViewController() as? AlbumsListViewController else {
+        fatalError("Can not load albums list view controller")
+    }
+    let flowCoordinator = AlbumsListFlowCoordinator()
+    let viewModel = AlbumsListViewModel(flowCoordinator : flowCoordinator)
+    albumsViewController.viewModel = viewModel
+    flowCoordinator.presenter = albumsViewController
+    flowCoordinator.navigator = navigator
+    return albumsViewController
+    }
+}
