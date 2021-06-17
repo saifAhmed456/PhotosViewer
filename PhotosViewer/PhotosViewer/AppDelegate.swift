de030efa0be9137dedaa8c03b97bdd77e35b11e4
saif ahmed <asshaik@corp.untd.com>
2021-06-17 11:05:02 +0530

    Load the initial view controller programatically

--- PhotosViewer/PhotosViewer/AppDelegate.swift
+++ PhotosViewer/PhotosViewer/AppDelegate.swift
@@ -16,9 +16,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        setupRootViewController()
         return true
     }
-
+    func setupRootViewController() {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let navigationController = UINavigationController()
+        let albumsController = AlbumsListFlowCoordinator.prepareAlbumsViewController(with: navigationController)
+        navigationController.viewControllers = [albumsController]
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
